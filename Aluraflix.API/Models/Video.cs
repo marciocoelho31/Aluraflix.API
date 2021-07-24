05755e5bb105d7c5fb1ca6e573f1e0908c8825c7
Marcio Coelho Socorro <marcioc@gmail.com>
2021-07-24 11:48:36 -0300

    Validação da propriedade da URL do video no modelo Video

--- Aluraflix.API/Models/Video.cs
+++ Aluraflix.API/Models/Video.cs
@@ -11,12 +11,16 @@ namespace Aluraflix.API.Models
         [Key]
         [Required(ErrorMessage = "O id do vídeo é obrigatório.")]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "O título do vídeo é obrigatório.")]
         [MaxLength(100, ErrorMessage = "O tamanho máximo do título do vídeo é de 100 caracteres.")]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "A descrição do vídeo é obrigatória.")]
         public string Descricao { get; set; }
+
         [Required(ErrorMessage = "A URL do vídeo é obrigatória.")]
+        [Url(ErrorMessage = "URL do vídeo inválida")]
         public string Url { get; set; }
     }
 }
